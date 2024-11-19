446ced7a86426fe0a05dbfda712bdbeeb6fb1679
Nitin382 <91834111+Nitin382@users.noreply.github.com>
2024-11-19 14:30:58 +0530

    Add files via upload

---
+++ w14p8.cpp
@@ -0,0 +1,28 @@
+//Q8. Program with multiple catch statements
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter number to check whether it's even or not: "; cin>>n;
+    try
+    {
+        if(n==0)
+        throw runtime_error("Error: number entered is 0!");
+        else if(n<0)
+        throw runtime_error("Error: negative number entered!");
+        else if(n%2!=0)
+        throw invalid_argument("Error: odd number entered!");
+        else cout<<"It's a even number!";
+    }
+    catch(const runtime_error& e)
+    {
+        cout<<e.what()<<endl;
+    }
+    catch(const invalid_argument& f)
+    {
+        cout<<f.what()<<endl;
+    }
+    return 0;
+}
